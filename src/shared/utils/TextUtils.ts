0253d4cf86c10e81ec566c2057a05d81e40574cb
Pablo Godoy <pablo.godoy@itti.digital>
2024-11-25 22:18:40 -0300

    fix: Remove DOMPurity for comptability reasons

--- src/shared/utils/TextUtils.ts
+++ src/shared/utils/TextUtils.ts
@@ -1,26 +1,21 @@
-
-import validator from 'validator'
-import { JSDOM } from 'jsdom'
-import DOMPurify from 'dompurify'
-const { window } = new JSDOM('<!DOCTYPE html>')
-const domPurify = DOMPurify(window)
+import validator from 'validator';
 
 export class TextUtils {
-
-  public static sanitize (unsafeText: string): string {
-    return domPurify.sanitize(unsafeText);
+  public static sanitize(unsafeText: string): string {
+    return validator.isEmail(unsafeText) ? unsafeText : unsafeText;
   }
 
-  public static validateWebURL (url: string): boolean {
+  public static validateWebURL(url: string): boolean {
     return validator.isURL(url);
   }
 
-  public static validateEmailAddress (email: string) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  public static validateEmailAddress(email: string) {
+    const re =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
 
-  public static createRandomNumericString (numberDigits: number): string {
+  public static createRandomNumericString(numberDigits: number): string {
     const chars = '0123456789';
     let value = '';
 
