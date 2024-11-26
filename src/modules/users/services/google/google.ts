aac062c3eb1993efd4d561cd5dbb65be4b96c3dc
Pablo Godoy <godoypablom@gmail.com>
2024-11-26 00:34:55 -0300

    feat: Create google authentication service

---
+++ src/modules/users/services/google/google.ts
@@ -0,0 +1,25 @@
+// node_modules
+import admin from 'firebase-admin';
+import { getAuth } from 'firebase-admin/auth';
+import dotenv from 'dotenv';
+import { readFileSync } from 'fs';
+
+dotenv.config();
+
+const serviceAccountKeys = readFileSync(process.env.SERVICE_ACCOUNT_KEYS, { encoding: 'utf8' });
+
+const app = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKeys as admin.ServiceAccount),
+});
+
+const auth = getAuth(app);
+
+export async function verifyGoogleToken(token: string) {
+  try {
+    const decodedToken = await auth.verifyIdToken(token);
+    return decodedToken;
+  } catch (error) {
+    const err = error;
+    throw err;
+  }
+}
---
+++ src/modules/users/services/google/googleAuthService.ts
@@ -0,0 +1,32 @@
+import { verifyGoogleToken } from './google';
+
+interface UserInfo {
+  id?: string;
+  email?: string;
+}
+
+export async function getSocialUserInfo(
+  accessToken: string,
+  provider: 'GOOGLE',
+  nonce: string | null
+): Promise<UserInfo | null> {
+  let userInfo: UserInfo | null = null;
+  switch (provider) {
+    case 'GOOGLE': {
+      const profile = await verifyGoogleToken(accessToken);
+
+      if (profile != null) {
+        userInfo = {
+          id: profile.sub,
+          email: profile.email,
+        };
+      }
+      break;
+    }
+    default:
+      userInfo = null;
+      break;
+  }
+
+  return userInfo;
+}
