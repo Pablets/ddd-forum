6da65a1084fecd049c28c79b344fcdca76a97bc5
Pablo Godoy <godoypablom@gmail.com>
2024-11-26 00:34:30 -0300

    feat: Add socialAccessToken in mapper

--- src/modules/users/mappers/userMap.ts
+++ src/modules/users/mappers/userMap.ts
@@ -1,42 +1,50 @@
-
-import { Mapper } from '../../../shared/infra/Mapper'
+import { Mapper } from '../../../shared/infra/Mapper';
 import { User } from '../domain/user';
 import { UserDTO } from '../dtos/userDTO';
 import { UniqueEntityID } from '../../../shared/domain/UniqueEntityID';
 import { UserName } from '../domain/userName';
 import { UserPassword } from '../domain/userPassword';
 import { UserEmail } from '../domain/userEmail';
+import { SocialAccessToken } from '../domain/socialAccessToken';
+import { getSocialUserInfo } from '../services/google/googleAuthService';
 
 export class UserMap implements Mapper<User> {
-  public static toDTO (user: User): UserDTO {
+  public static toDTO(user: User): UserDTO {
     return {
       username: user.username.value,
       isEmailVerified: user.isEmailVerified,
       isAdminUser: user.isAdminUser,
-      isDeleted: user.isDeleted
-    }
+      isDeleted: user.isDeleted,
+    };
   }
 
-  public static toDomain (raw: any): User {
+  public static toDomain(raw: any): User {
     const userNameOrError = UserName.create({ name: raw.username });
     const userPasswordOrError = UserPassword.create({ value: raw.user_password, hashed: true });
     const userEmailOrError = UserEmail.create(raw.user_email);
+    const socialAccessTokenOrError = SocialAccessToken.create(raw.social_access_token);
 
-    const userOrError = User.create({
-      username: userNameOrError.getValue(),
-      isAdminUser: raw.is_admin_user,
-      isDeleted: raw.is_deleted,
-      isEmailVerified: raw.is_email_verified,
-      password: userPasswordOrError.getValue(),
-      email: userEmailOrError.getValue(),
-    }, new UniqueEntityID(raw.base_user_id));
+    const userOrError = User.create(
+      {
+        username: userNameOrError.getValue(),
+        isAdminUser: raw.is_admin_user,
+        isDeleted: raw.is_deleted,
+        isEmailVerified: raw.is_email_verified,
+        email: userEmailOrError.getValue(),
+        password: userPasswordOrError.isSuccess ? userPasswordOrError.getValue() : null,
+        socialAccessToken: socialAccessTokenOrError.isSuccess
+          ? socialAccessTokenOrError.getValue()
+          : null,
+      },
+      new UniqueEntityID(raw.base_user_id)
+    );
 
     userOrError.isFailure ? console.log(userOrError.getErrorValue()) : '';
 
     return userOrError.isSuccess ? userOrError.getValue() : null;
   }
 
-  public static async toPersistence (user: User): Promise<any> {
+  public static async toPersistence(user: User): Promise<any> {
     let password: string = null;
     if (!!user.password === true) {
       if (user.password.isAlreadyHashed()) {
@@ -46,6 +54,17 @@ export class UserMap implements Mapper<User> {
       }
     }
 
+    let socialId: string = null;
+    // TODO: Find a better place to do this validation
+    if (!!user.socialAccessToken) {
+      const getUserSocialInfo = await getSocialUserInfo(
+        user.socialAccessToken.value,
+        'GOOGLE',
+        null
+      );
+      socialId = getUserSocialInfo.id;
+    }
+
     return {
       base_user_id: user.userId.getStringValue(),
       user_email: user.email.value,
@@ -53,7 +72,8 @@ export class UserMap implements Mapper<User> {
       username: user.username.value,
       user_password: password,
       is_admin_user: user.isAdminUser,
-      is_deleted: user.isDeleted
-    }
+      is_deleted: user.isDeleted,
+      social_access_token: socialId,
+    };
   }
 }
